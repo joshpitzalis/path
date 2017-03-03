506b4c0b1f5185aa384ccd157bd1022f04943a58
aditya2337 <aditya.n@gai.co.in>
2017-03-03 13:54:55 +0530

    trying to get the redirect bug fixed

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AuthService from '../utils/AuthService';
-import { Link, Redirect } from 'react-router-dom';
+import { Link, Redirect, withRouter } from 'react-router-dom';
 import domain from '../utils/domain';
 
 const auth = new AuthService(process.env.REACT_APP_AUTH0_CLIENT_ID, process.env.REACT_APP_AUTH0_DOMAIN);
@@ -11,29 +11,50 @@ class Profile extends React.Component {
     this.state = {
       tutorials: [],
       user: auth.getProfile(),
-      refresh: false
+      refresh: false,
+      isFetching: true
     };
   }
 
   componentWillMount () {
+    console.log('mount');
     fetch(domain.server)
       .then(response => response.json())
       .then(response =>
-        this.setState({tutorials: response})
+        this.setState({tutorials: response, isFetching: false})
       );
   }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps);
+    this.render();
+    if (nextProps.location.state) {
+      this.setState({refresh: false})
+      console.log('falsed');
+    }
+  }
+
+  timeoutPromise = (timeout, err, promise) => {
+    return new Promise(function(resolve,reject) {
+        promise.then(resolve,reject);
+        setTimeout(reject.bind(null,err), timeout);
+      });
+    }
 
   handleDelete = (id) => {
     auth.fetch(`${domain.server}/api/delete?id=${id}`,
       {method: 'DELETE'})
-      .then(
-        this.setState({
-        refresh: true
+    .then(this.setState({isFetching: true}))
+    fetch(domain.server)
+      .then(response => response.json())
+      .then(response => {
+        setTimeout(function() { this.setState({tutorials: response, isFetching: false}); }.bind(this), 1000);
+        console.log(this.state);
       })
-    )
   }
 
   render () {
+    const { from } = this.props.location.state || { from: { pathname: '/profile' } }
     const tutorials = this.state.tutorials
     .filter(tut => tut.id === this.state.user.user_id)
     .map((tut, index) => <article key={index} className='w5 bg-white br3 pa3 pa4-ns ma1 ba b--black-10 tc' >
@@ -50,7 +71,16 @@ class Profile extends React.Component {
 
     if (this.state.refresh) {
       return (
-        <Redirect to='/' />
+        <Redirect to={{
+          pathname: '/profile',
+          state: { referrer: from }
+        }} />
+      );
+    }
+
+    if (this.state.isFetching) {
+      return (
+        <h1>Loading...</h1>
       );
     }
 
@@ -66,4 +96,4 @@ class Profile extends React.Component {
   }
 }
 
-export default Profile;
+export default withRouter(Profile);
